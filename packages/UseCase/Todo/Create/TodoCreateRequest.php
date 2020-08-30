d67ab75b81bd355067a5f4560f968f5a53f1837e
KogaYuta <koga-yuta-np@ynu.jp>
2020-08-30 17:22:08 +0900

    create DataStructure for create response

---
+++ packages/UseCase/Todo/Create/TodoCreateRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace packages\UseCase\Todo\Create;
+
+class TodoCreateRequest {
+    /**
+     * @var string
+     */
+    private $title;
+    /**
+     * @var DateTime
+     */
+    private $limit;
+    /**
+     * @var bool
+     */
+    private $completed;
+
+    public function __construct(string $title, DateTime $limit, bool $completed)
+    {
+        $this->title = $title;
+        $this->limit = $limit;
+        $this->completed = $completed;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getLimit(): DateTime
+    {
+        return $this->limit;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getCompleted(): bool
+    {
+        return $this->completed;
+    }
+}
